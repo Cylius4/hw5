d996cc2666efc2f7a1155ec5a98c1f1305921f77
Wenbo Zhang <wenboz@usc.edu>
2024-04-17 22:40:42 -0700

    Finished

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,16 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool scheduleHelper(
+        const AvailabilityMatrix& avail,
+        const size_t dailyNeed,
+        const size_t maxShifts,
+        DailySchedule& sched,
+        vector<size_t>& workerShifts,
+        vector<vector<bool>>& occupied,
+        size_t day,
+        size_t slot
+);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -35,11 +44,46 @@ bool schedule(
     if(avail.size() == 0U){
         return false;
     }
+
     sched.clear();
-    // Add your code below
-
-
-
-
+    sched.resize(avail.size(), vector<Worker_T>(dailyNeed, INVALID_ID));
+    vector<size_t> workerShifts(avail[0].size(), 0);
+    vector<vector<bool>> occupied(avail.size(), vector<bool>(avail[0].size(), false));
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, workerShifts, occupied, 0, 0);
 }
 
+bool scheduleHelper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<size_t>& workerShifts,
+    vector<vector<bool>>& occupied,
+    size_t day,
+    size_t slot
+){
+    // Base case
+    if(day == avail.size()){
+        return true;
+    }
+    // Recursive case
+    for(size_t worker = 0; worker < avail[day].size(); worker++){
+        if(avail[day][worker] && workerShifts[worker] < maxShifts && !occupied[day][worker]){
+            sched[day][slot] = worker;
+            workerShifts[worker]++;
+            occupied[day][worker] = true;
+            if (slot == dailyNeed - 1){
+                if(scheduleHelper(avail, dailyNeed, maxShifts, sched, workerShifts, occupied, day + 1, 0)){
+                    return true;
+                }
+            } else {
+                if(scheduleHelper(avail, dailyNeed, maxShifts, sched, workerShifts, occupied, day, slot + 1)){
+                    return true;
+                }
+            }
+            workerShifts[worker]--;
+            occupied[day][worker] = false;
+        }
+    }
+    return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+std::set<std::string> dfs(std::string& in, std::string floating, const std::set<std::string>& dict, size_t, int);
+bool isValid(const std::string& word, const std::string& floating, const std::set<std::string>& dict);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -21,8 +22,80 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
-
+    string inCopy = in;
+    int numSpace = 0;
+    for (char c : in)
+    {
+        if (c == '-')
+        {
+            numSpace++;
+        }
+    }
+    return dfs(inCopy, floating, dict, numSpace, 0);
 }
 
 // Define any helper functions here
+bool isValid(const std::string& word, const std::string& floating, const std::set<std::string>& dict) {
+    return dict.find(word) != dict.end();
+}
+
+std::set<std::string> dfs(std::string& in, string floating, const std::set<std::string>& dict, size_t numSpace, int usedFloating)
+{
+    // We can use discretization (vector storing the index of the '-'s in the string) to optimize
+    // but there are only a few '-' and the guide didn't say we can't use string::find
+    // so I'm lazy
+    size_t pos = in.find('-');
+    // Base case
+    // no '-'
+    if (pos == string::npos)
+    {
+        if (isValid(in, floating, dict))
+        {
+            return {in};
+        }
+        else
+        {
+            return {};
+        }
+    }
+    // Recursive case
+    set <string> result;
+    if (numSpace > floating.size() - usedFloating) {
+        for (char c = 'a'; c <= 'z'; c++) {
+            // can instead use a used array to optimize
+            // but I'm lazy
+            size_t fpos = floating.find(c);
+            in[pos] = c;
+            char used_f = floating[fpos];
+            if (fpos != string::npos) {
+                floating[fpos] = '-';
+                usedFloating++;
+            }
+            set<string> temp = dfs(in, floating, dict, numSpace - 1, usedFloating);
+            result.insert(temp.begin(), temp.end());
+            if (fpos != string::npos) {
+                usedFloating--;
+                floating[fpos] = used_f;
+            }
+        }
+    }
+    // if we must use floating characters
+    // extracted to optimize efficiency
+    else {
+        for (size_t fpos = 0; fpos < floating.size(); fpos++) {
+            if (floating[fpos] == '-') {
+                continue;
+            }
+            in[pos] = floating[fpos];
+            char used_f = floating[fpos];
+            floating[fpos] = '-';
+            usedFloating++;
+            set<string> temp = dfs(in, floating, dict, numSpace - 1, usedFloating);
+            result.insert(temp.begin(), temp.end());
+            usedFloating--;
+            floating[fpos] = used_f;
+        }
+    }
+    in[pos] = '-';
+    return result;
+}
